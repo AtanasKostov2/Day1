ef2531aaf82634b50f89f967d033bd6e77c7c2c6
fmi <fmi@fmi.uni-plovdiv.bg>
2022-04-07 15:33:58 +0300

    Add project files.

---
+++ Ablajajajajjaja/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ablajajajajjaja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proj1!");
+
+
+        }
+    }
+}
---
+++ SecondProj/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SecondProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value:");
+            //int number = int.Parse(Console.ReadLine());
+            int num;
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine($"Entered number {num}");
+            }
+            else
+            {
+                Console.WriteLine($"Wrong input");
+
+            }
+
+        }
+    }
+}
